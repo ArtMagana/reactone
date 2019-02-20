c88697c90980c3da5218423fe52a01b22e7a97c8
Arturo <arturo.magana@shq.com.mx>
2019-02-19 20:45:16 -0600

    class

--- src/App.js
+++ src/App.js
@@ -1,2 +1,12 @@
 import React, { Component } from 'react'
 import './App.css'
+
+class App extends Component {
+  render() {
+    return(
+      <div className="App">
+        <h1>Hola a toda la banda</h1>
+      </div>
+    )
+  }
+}
